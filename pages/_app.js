54e8d07e4bc2686b231c70bce3035aa57a6c81f6
isaiah749 <isaiah749@gmail.com>
2022-05-04 11:49:25 -0400

    adding pixel part 2

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,7 @@ import Script from 'next/script';
 import { useRouter } from 'next/router';
 import Router from 'next/router';
 import * as gtag from '../lib/gtag';
+import * as fbq from '../lib/fpixel'
 
 // function MyApp({ Component, pageProps }) {
 // 	return (
@@ -53,19 +54,32 @@ const App = ({ Component, pageProps }) => {
 
 
 
+  // useEffect(() => {
+  //   import('react-facebook-pixel')
+  //   .then((x) => x.default)
+  //   .then((ReactPixel) => {
+  //     ReactPixel.init('4957337474386732')
+  //     ReactPixel.pageView()
+
+  //     router.events.on('routeChangeComplete', () => {
+  //       ReactPixel.pageView()
+  //     })
+  //   })
+  // }, [router.events])
+
   useEffect(() => {
-    import('react-facebook-pixel')
-    .then((x) => x.default)
-    .then((ReactPixel) => {
-      ReactPixel.init('4957337474386732')
-      ReactPixel.pageView()
+    // This pageview only triggers the first time (it's important for Pixel to have real information)
+    fbq.pageview()
 
-      router.events.on('routeChangeComplete', () => {
-        ReactPixel.pageView()
-      })
-    })
+    const handleRouteChange = () => {
+      fbq.pageview()
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
   }, [router.events])
-
 
   useEffect(() => {
     const handleRouteChange = (url) => {
@@ -96,6 +110,23 @@ const App = ({ Component, pageProps }) => {
             gtag('config', '${gtag.GA_TRACKING_ID}', {
               page_path: window.location.pathname,
             });
+          `,
+        }}
+      />
+            {/* Global Site Code Pixel - Facebook Pixel */}
+            <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', ${fbq.FB_PIXEL_ID});
           `,
         }}
       />
